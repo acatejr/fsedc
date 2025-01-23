3f89494ebbfee5cf226a7b1bfa27bff125492de9
acatejr <acatejr@gmail.com>
2025-01-23 16:35:48 -0700

    Setup postgresql with vector db

--- database.py
+++ database.py
@@ -3,8 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Database configuration
-DATABASE_URL = "sqlite:///catalog.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+DATABASE_URL = ""
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
